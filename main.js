700ef32fd14285b2788cfe03cd5067c1e793fab1
hossainahmed359 <hossainahmed359@gmail.com>
2023-08-19 03:41:58 +0600

    Add search bard design

--- main.js
+++ main.js
@@ -1,8 +1,32 @@
 import './styles/main.scss';
 import { testScript } from './script/test.js';
+import { searchScript } from './script/search';
 
 
-console.log('Main Script Runnig!');
-console.log(testScript());
+// console.log('Main Script Runnig!');
+// console.log(testScript());
+
+let isSearching = false;
+const searchBtn = document.getElementById('search-btn');
+const navMenu = document.getElementById('nav-bottom-menu');
+
+
+
+
+searchBtn.addEventListener('click', handleSearchClick) ;
+
+function handleSearchClick () {
+     // Show Input Field And Show Cancel Button
+    if(isSearching === false) {
+        navMenu.classList.add('search-active');
+    } else {
+        navMenu.classList.remove('search-active');
+    }
+
+    isSearching = !isSearching;
+    
+   
+
+}
 
 
---
+++ script/search.js
@@ -0,0 +1,3 @@
+export function searchScript () {
+    console.log('clicked Search')
+}
