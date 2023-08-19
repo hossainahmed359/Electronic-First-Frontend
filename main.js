a30227279e18bd830d518f56db330f88a158af76
hossainahmed359 <hossainahmed359@gmail.com>
2023-08-20 01:54:13 +0600

    Add use Add event

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 import './styles/main.scss';
 import useSearch from './script/useSearch';
 import useDropdown from './script/useDropdown';
+import useAddEvent from './script/useAddEvent';
+
 const { handleSearchOn, handleSearchClose, handleMoblieSearchOn, handleMobileSearchClose } = useSearch();
 const { handleDropdownShow, handleDropdownClose } = useDropdown();
+const { addEventToMultitpleElements } = useAddEvent();
 
 
 // STATES
@@ -10,14 +13,10 @@ let isSearching = false;
 let isDropdownVisible = false;
 
 
-// SEARCH
-const searchBtn = document.getElementById('search-btn');
-searchBtn.addEventListener('click', handleSearchClick);
-
 function handleSearchClick() {
     if (isSearching === false) {
         handleSearchOn();
-        if(isDropdownVisible) {
+        if (isDropdownVisible) {
             handleDropdownClose();
         }
     } else {
@@ -26,25 +25,6 @@ function handleSearchClick() {
     isSearching = !isSearching;
 }
 
-// MOBILE SEARCH
-const mobileSearchBtn = document.getElementById('mobile-search-btn');
-const mobileSearchCancelBtn = document.getElementById('mobile-search-cancel-btn');
-
-
-
-mobileSearchBtn.addEventListener('click', handleMoblieSearchOn);
-mobileSearchCancelBtn.addEventListener('click', handleMobileSearchClose);
-
-
-
-// DROPDOWN
-
-const dropdownButtonList = document.querySelectorAll('.nav__contents__menus__bottom__dropdown-list__item');
-
-dropdownButtonList.forEach((element, index) => {
-    element.id = `dropdown-button-${index + 1}`
-    element.addEventListener('click', toggleDropdown);
-});
 
 function toggleDropdown() {
     if (isDropdownVisible === false) {
@@ -55,3 +35,29 @@ function toggleDropdown() {
 
     isDropdownVisible = !isDropdownVisible;
 }
+
+
+const arrayOfListedElements = [
+    {
+        id: 'search-btn',
+        event: 'click',
+        callback: handleSearchClick
+    },
+    {
+        id: 'mobile-search-btn',
+        event: 'click',
+        callback: handleMoblieSearchOn
+    },
+    {
+        id: 'mobile-search-cancel-btn',
+        event: 'click',
+        callback: handleMobileSearchClose
+    },
+    {
+        className: 'nav__contents__menus__bottom__dropdown-list__item',
+        event: 'click',
+        callback: toggleDropdown
+    },
+]
+
+addEventToMultitpleElements(arrayOfListedElements)
